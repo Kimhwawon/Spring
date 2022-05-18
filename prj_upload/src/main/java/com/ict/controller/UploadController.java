324294c86b5013240e2581787be2540ae066abac
Kimhwawon <ghktlrekd@naver.com>
2022-05-18 09:09:43 +0900

    파일 올리기

--- prj_upload/src/main/java/com/ict/controller/UploadController.java
+++ prj_upload/src/main/java/com/ict/controller/UploadController.java
@@ -7,8 +7,10 @@ import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.ict.domain.AttachFileDTO;
 
 import lombok.extern.log4j.Log4j;
 import net.coobird.thumbnailator.Thumbnailator;
@@ -59,7 +63,7 @@ public class UploadController {
 	@PostMapping(value="/uploadFormAction", 
 				produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
-	public void uploadFormPost(MultipartFile[] uploadFile) {
+	public ResponseEntity<List<AttachFileDTO>> uploadFormPost(MultipartFile[] uploadFile) {
 		
 		// AttachFileDTO는 파일 한 개의 정보를 저장합니다.
 		// 현재 파일 업로드는 여러 파일을 동시에 업로드하므로 List<AttachFileDTO>를 받도록 합니다.
@@ -67,6 +71,9 @@ public class UploadController {
 		
 		// 어떤 폴더에 저장할것인지 위치지정
 		String uploadFolder = "C:\\upload_data\\temp";
+		
+		// 폴더 경로 받아오기
+		String uploadFolderPath = getFolder();
 		
 		// 폴더생성 (폴더 유무체크)
 				File uploadPath = new File(uploadFolder, getFolder());
@@ -82,18 +89,22 @@ public class UploadController {
 			log.info("Upload File Name: " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size: " + multipartFile.getSize());
 			
+			// 파일정보를 저장할 DTO생성
+			AttachFileDTO attachDTO = new AttachFileDTO();
+			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 			log.info("last file name : " + uploadFileName);
+			
+			// 상단에 만든 dto에 파일이름 저장
+			attachDTO.setFileName(uploadFileName);
 			
 			// uuid 발급 부분
 			UUID uuid = UUID.randomUUID();
 						
 			uploadFileName = uuid.toString() + "-" + uploadFileName;
 		
-			
-			
 		//	File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
 		//	File saveFile = new File(uploadPath,uploadFileName);
 			
@@ -101,19 +112,27 @@ public class UploadController {
 				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
 				
+				// uuid와 저장할 폴더 경로는 setter로 입력받기
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+				
 				// 이 아래부터 썸네일 생성로직
 				if(checkImageType(saveFile)) {
+					attachDTO.setImage(true); // 이미지가 트루라면
+					
 					FileOutputStream thumbnail = 
 							new FileOutputStream(new File(uploadPath, "s_"+uploadFileName));
 					
 					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
 					thumbnail.close();					
 				}
+				// ArrayList에 개별 DTO를 집어넣어줘야 출력됨
+				list.add(attachDTO);
 			} catch(Exception e) {
 				log.error(e.getMessage());
 			}			
 		} // end for
-		
+		return new  ResponseEntity<>(list, HttpStatus.OK);
 	}
 	
 	@GetMapping("/uploadAjax")
@@ -121,14 +140,24 @@ public class UploadController {
 		log.info("upload ajax");
 	}
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+	
+	// uploadFormAction과 유사하게 AttachDTO를 받도록 32~35를 보고 수정해주세요.
+	@PostMapping(value="/uploadAjaxAction",
+			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody //rest 큰트롤러 메서드로 변경
+	public ResponseEntity<List<AttachFileDTO>>uploadAjaxPost(MultipartFile[] uploadFile) {
+		
 		log.info("ajax post update!");
+		
+		List<AttachFileDTO> list = new ArrayList<>();
 		
 		String uploadFolder = "C:\\upload_data\\temp";
 		
+		
+		String uploadFolderPath = getFolder();
+		
 		// 폴더생성 (폴더 유무체크)
-		File uploadPath = new File(uploadFolder, getFolder());
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
 		log.info("upload path : " + uploadPath);
 		
 		if(uploadPath.exists() == false) {
@@ -141,11 +170,16 @@ public class UploadController {
 			log.info("Upload file name : " + multipartFile.getOriginalFilename());
 			log.info("Upload file siza : " + multipartFile.getSize());
 			
+			// 개별 파일에 대한 DTO 생성
+			AttachFileDTO attachDTO = new AttachFileDTO();
+			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			
-			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
-			
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);			
 			log.info("last file name : " + uploadFileName);
+			
+			// uploadFileNAMe에 의해 파일이름을 얻어왔으면 파일명을 attachDTO에 집어넣음
+			attachDTO.setFileName(uploadFileName);
 			
 			// uuid 발급 부분
 			UUID uuid = UUID.randomUUID();
@@ -160,18 +194,28 @@ public class UploadController {
 				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
 				
+				// uuid와 저장할 폴더 경로는 setter로 입력받기
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+				
 				// 이 아래부터 썸네일 생성로직
 				if(checkImageType(saveFile)) {
+					attachDTO.setImage(true);
+					
 					FileOutputStream thumbnail = 
 							new FileOutputStream(new File(uploadPath, "s_"+uploadFileName));
 					
 					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
 					thumbnail.close();					
 				}
+				// ArrayList에 개별 DTO를 집어넣어줘야 출력됨
+				list.add(attachDTO);
+				
 			} catch(Exception e) {
 				log.error(e.getMessage());
 			}			
 		} // end for
+		return new  ResponseEntity<>(list, HttpStatus.OK);
 		
 	}
 	
--- prj_upload/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ prj_upload/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -3,6 +3,26 @@
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+	.uploadResult {
+		width:100%;
+		background-color:lightyellow;
+	}
+	.uploadResult ul {
+		display:flex;
+		flex-flow:row;
+		justify-content:center;
+		align-items: center;
+	}
+	.uploadResult ul li{
+		list-style:none;
+		padding: 10px;
+	}
+	.uploadResult ul li img{
+		width:20px;
+	}
+
+</style>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
@@ -11,6 +31,12 @@
 	
 	<div class="uploadDiv">
 		<input type="file" name="uploadFile" multiple>
+	</div>
+	
+	<div class="uploadResult">
+		<ul>
+			<!-- 업로드된 파일이 들어갈 자리 -->
+		</ul>
 	</div>
 	
 	<button id="uploadBtn">Upload</button>
@@ -36,7 +62,10 @@
 			}
 			return true;
 		}
-			
+			// 첨부가 안 된 상태의 .uploadDiv를 깊은 복사해서 
+			// 첨부 완료 후에 안 된 시점의 uploadDiv로 덮어씌우기
+			var cloneObj = $(".uploadDiv").clone();	
+		
 			$('#uploadBtn').on("click", function(e){
 					
 				
@@ -62,13 +91,33 @@
 					contentType: false,
 					data: formData,
 					type: 'POST',
+					dataType:'json',
 					success : function(result){
+						console.log(result);
+						showUploadedFile(result);
+					// 업로드 성공시 미리 복사해둔 uploadDiv 로 덮어씌워서 첨부파일이 없는 상태로 되돌려 놓기
+						$(".uploadDiv").html(cloneObj.html());
 						alert("Uploaded");
+						
 					}
 				}); //ajax
 			});
 			
-		});
+		}); //onclick uploadBtn
+		var uploadResult = $(".uploadResult ul");
+		
+		function showUploadedFile(uploadResultArr){
+			var str="";
+			
+			$(uploadResultArr).each(function(i, obj){
+				if(!obj.image){
+				str += "<li><img src='/resources/free-icon-attached-file-1209914.png'>"+ obj.fileName + "</li>";
+				}else{
+					str += "<li>" + obj.fileName + "</li>";
+				}
+			});
+			uploadResult.append(str);
+		}// showUploadedfile
 		
 		
 	</script>
